69006d269a5f968009a011551f90e5dfbaf29924
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-06 21:49:02 +0700

    add signin feature

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,19 @@
 const { sequelize } = require('../models/db');
 const { UserDbService } = require('../services/db');
+const { compare, generateAccessToken } = require('../utils');
 
 class AuthController {
   static async register(req, res) {
-    const { name, email, password } = req.body;
     const transaction = await sequelize.transaction();
 
     try {
+      const { name, email, password } = req.body;
       const user = await UserDbService.findOneUserByEmail(email);
 
       if (user) {
         return res.status(400).json({
           code: res.statusCode,
-          status: 'Email is already registered',
+          status: 'Email is Already Registered',
         });
       }
 
@@ -31,10 +32,32 @@ class AuthController {
 
   static async signin(req, res) {
     try {
+      const { email, password } = req.body;
+      const user = await UserDbService.findOneUserByEmail(email);
+
+      if (user === null) {
+        return res.status(404).json({
+          code: res.statusCode,
+          status: 'Email Not Found',
+        });
+      }
+
+      const isMatch = await compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json({
+          code: res.statusCode,
+          status: 'Invalid Password',
+        });
+      }
+
+      const accessToken = generateAccessToken({ id: user.id, name: user.name, email: user.email });
+
       res.status(201).json({
         code: res.statusCode,
         status: 'Created',
-        data: {},
+        data: {
+          accessToken,
+        },
       });
     } catch (err) {
       res.sendStatus(500).end();
